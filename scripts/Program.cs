df87dd643eae9d239b72e5a2c80942baa4c846e3
Nathan-Andrews <andrewsnathan2003@gmail.com>
2024-08-03 20:58:51 -0500

    changed name of some shaders

--- scripts/Program.cs
+++ scripts/Program.cs
@@ -2,7 +2,10 @@
 
 class Program {
     public static void Main(string[] args) {
-        using (var window = new NearestPointRender(800, 800, "Nearest Point Visualization","nearest_point_2nd_fill")) {
+        // options:
+        string shadername = "nearest_point_rand_color"; // "nearest_point_rand_color", "nearest_point_graident_fill", "nearest_point_edge_distance", "second_nearest_point"
+
+        using (var window = new PointVisualizer(800, 800, "Nearest Point Visualization",shadername)) {
             window.Run();
         }
     }
